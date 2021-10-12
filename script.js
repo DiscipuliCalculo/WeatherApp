0094d1bd2d2934d9ce3d97e176cad781b61615d8
Vitam Discipuli <discipulicalculo@gmail.com>
2021-10-11 17:03:15 -0700

    added functionality to display data

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-function fetchWeatherData(location, callback) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`, { mode: 'cors' })
+document.getElementById('submit-location').addEventListener('click', () => {
+  handleSubmit();
+});
+
+function handleSubmit() {
+  fetchWeatherData(locationInput(), processData, displayData);
+}
+
+function fetchWeatherData(location, callback1, callback2) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=`, { mode: 'cors' })
     .then((response) => response.json())
-    .then((data) => callback(data));
-}
-
-function displayData(data) {
-  console.log(data);
-}
-
-function processData(data) {
-  const weatherData = {
-    sky: data.weather[0].description,
-    temp: data.main.temp,
-    temp_min: data.main.temp_min,
-    temp_max: data.main.temp_max,
-    feels_like: data.main.feels_like,
-    humidity: data.main.humidity,
-  };
-  console.log(weatherData);
-  return weatherData;
+    .then((data) => callback1(data))
+    .then((data) => {
+      callback2(data);
+    });
 }
 
 function locationInput() {
@@ -26,6 +20,25 @@ function locationInput() {
   return location;
 }
 
-document.getElementById('submit-location').addEventListener('click', () => {
-  console.log(fetchWeatherData(locationInput(), processData));
-});
+function processData(data) {
+  const weatherData = {
+    location: data.name,
+    sky: data.weather[0].main,
+    sky_desc: data.weather[0].description,
+    temp: data.main.temp,
+    temp_min: data.main.temp_min,
+    temp_max: data.main.temp_max,
+    feels_like: data.main.feels_like,
+    humidity: data.main.humidity,
+  };
+  return weatherData;
+}
+
+function displayData(data) {
+  document.getElementById('weather-data').innerHTML = `
+	<span class="data-item">${data.location}</span>
+	<span class="data-item">${data.temp}</span>
+	<span class="data-item">${data.humidity}</span>
+	<span class="data-item">${data.sky_desc}</span>
+  `;
+}
